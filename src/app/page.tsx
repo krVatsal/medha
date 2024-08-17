b30380f443d2a3380e0145f3d6d45a4909c93d46
abhigit-saha <abhijitsaha1006@gmail.com>
2024-08-17 19:03:17 +0530

    Fixed undo and redo and changed the interface

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
 import Whiteboard from "./whiteboard/page";
+import DrawingTool from "./canvas/page";
+import Penciltool from "./canvas/pencilTool/page";
+import Canvas from "./canvas/page";
 export default function Home() {
   return (
-<div>
-  <Whiteboard/>
-</div>
+    <div>
+      <Canvas />
+    </div>
   );
 }
