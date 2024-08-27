8a70a7e6bf0b182f63fdc950463edee05ef13703
KrVatsal34 <kumarvatsal34@gmail.com>
2024-08-27 19:00:01 +0530

    fixes for production

--- src/app/utils/dynamic.tsx
+++ src/app/utils/dynamic.tsx
@@ -1,23 +1,37 @@
 "use client";
 import React from "react";
-import Excalidraw, {
-  convertToExcalidrawElements,
-  ExcalidrawProps,
-} from "@excalidraw/excalidraw";
-
+import Excalidraw, { ExcalidrawProps, ExcalidrawElement } from "@excalidraw/excalidraw";
 import "@excalidraw/excalidraw/index.css";
 
 const ExcalidrawWrapper: React.FC = () => {
-  const elements = convertToExcalidrawElements([
+  // Define elements with the required properties
+  const elements: ExcalidrawElement[] = [
     {
       type: "rectangle",
+      version: 1,
+      versionNonce: 0,
+      isDeleted: false,
       id: "rect-1",
+      fillStyle: "hachure",
+      strokeWidth: 1,
+      strokeStyle: "solid",
+      roughness: 1,
+      opacity: 100,
+      angle: 0,
+      x: 100,
+      y: 100,
+      strokeColor: "#000000",
+      backgroundColor: "transparent",
       width: 186.47265625,
       height: 141.9765625,
+      seed: 1,
+      groupIds: [],
+      boundElements: [],
+      updated: Date.now(),
+      link: null,
+      locked: false,
     },
-  ]);
-
-  console.info(elements);
+  ];
 
   return (
     <div style={{ height: "500px", width: "500px" }}>
